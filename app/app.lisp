29d7a30c3e8bd62fd7f6d943fa89db70c446808d
Christian <christianheyn@mailbox.org>
2021-10-09 14:23:23 +0200

    get first input vars

--- app/app.lisp
+++ app/app.lisp
@@ -1,16 +1,21 @@
 (defpackage :app
-    (:use :common-lisp)
-    (:export
-        #:main
-        ))
+  (:use :common-lisp)
+  (:export #:main))
 
 (in-package :app)
 
 (require "io" "./utils/io.lisp")
 (require "random" "./utils/random.lisp")
+(require "str" "./utils/str.lisp")
+
+(defun input-vars ()
+  (list
+    :width (io:ask-with-default "* width in px" "1080")
+    :height (io:ask-with-default "* height in px" "1080")
+    :max-elements (io:ask-with-default "* max elements" "123")))
 
 (defun main (args)
-    (progn
-        (io:output(io:ask "He?"))
-        (io:output (random:number-to 39))
-    ))
+  (io:output (str:cyan "First I need some infos
+for creating the artwork:
+_________________________"))
+  (io:output (input-vars)))
--- utils/str.lisp
+++ utils/str.lisp
@@ -2,6 +2,12 @@
     (:use :common-lisp)
     (:export
         #:concat-all
+        #:red
+        #:blue
+        #:green
+        #:cyan
+        #:yellow
+        #:purple
         ))
 
 (in-package :str)
@@ -11,3 +17,27 @@
         ""
         (concatenate 'string (first xs)
             (apply #'concat-all (rest xs) ))))
+
+(defun red (str)
+    (format t
+     (concat-all "~c[31m" str "~c[0m~%") #\ESC #\ESC))
+
+(defun blue (str)
+    (format t
+     (concat-all "~c[34m" str "~c[0m~%") #\ESC #\ESC))
+
+(defun green (str)
+    (format t
+     (concat-all "~c[32m" str "~c[0m~%") #\ESC #\ESC))
+
+(defun cyan (str)
+    (format t
+     (concat-all "~c[36m" str "~c[0m~%") #\ESC #\ESC))
+
+(defun yellow (str)
+    (format t
+     (concat-all "~c[33m" str "~c[0m~%") #\ESC #\ESC))
+
+(defun purple (str)
+    (format t
+     (concat-all "~c[35m" str "~c[0m~%") #\ESC #\ESC))
